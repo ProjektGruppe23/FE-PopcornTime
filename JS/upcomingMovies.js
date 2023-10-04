0b720b2e89ad41981d4a0ea49e8b2b22faf27677
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-10-04 20:04:15 +0200

    Update styles + eventlistener
    - Updated styles for buttons and text
    - Eventlisteners for buttons and pictures

--- JS/upcomingMovies.js
+++ JS/upcomingMovies.js
@@ -2,24 +2,36 @@ document.addEventListener("DOMContentLoaded", function ()
 {
     // Replace this URL with your backend API URL
     const apiUrl = "http://localhost:8080/movies/upcoming";
+    const movieContainer = document.getElementById('movie-container');
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(movies =>
         {
-            const movieContainer = document.getElementById('movie-container');
-
             movies.forEach(movie =>
             {
                 const movieCard = `
                     <div class="movie-card">
                         <img src="${movie.picture}" alt="${movie.title}">
                         <div class="movie-title">${movie.title}</div>
-                        <button id="selectMovie" class="button-style" >select movie</button>
+                         <div class="movie-period">playing dates: Date -> Date</div>
+                        <button id="selectMovie" class="button-style" >Select movie <span id="arrow">&#8702</span></button>
                     </div>
                 `;
                 movieContainer.innerHTML += movieCard;
             });
         })
         .catch(error => console.error("Error fetching movies:", error));
+
+    movieContainer.addEventListener('click', function (event)
+    {
+        const target = event.target;
+        //const movieId = target.getAttribute('data-movieid');
+
+        if (target.tagName === 'BUTTON' || target.tagName === 'IMG')
+        {
+            window.location.href = "https://cinemaxx.dk/koebenhavn/kommende-film";
+            //`somepage.html?movie=${movieId}`;
+        }
+    });
 });
