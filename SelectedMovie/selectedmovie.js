d11d1cb2d74857d22972c5982d0b28c8357c74d8
Fred86r9 <118350827+Fred86r9@users.noreply.github.com>
2023-10-06 15:13:50 +0200

    Created js functions and html for selectedmovie

--- SelectedMovie/selectedmovie.js
+++ SelectedMovie/selectedmovie.js
@@ -1,16 +1,58 @@
-// Function to fetch a single movie by ID
-async function fetchMovieById(movieid) {
-    const apiUrl = `http://localhost:8080/selectedmovie/${movieid}`;
+async function fetchAnyUrl(url) {
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(url);
         if (response.ok) {
             return await response.json();
         } else {
-            console.error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+            throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
         }
     } catch (error) {
         console.error("Fetch Error:", error);
+        throw error;
     }
+}
+
+
+// Function to fetch a single movie by ID
+
+
+
+
+async function fetchMovieById(movieid) {
+    const apiUrl = `http://localhost:8080/selectedmovie/${movieid}`;
+
+        const movieById = await fetchAnyUrl(apiUrl);
+        return movieById;
+}
+
+async function fetchGenreById(movieid) {
+    const apiUrl = `http://localhost:8080/genres/${movieid}`;
+
+    const genreById = await fetchAnyUrl(apiUrl);
+    return genreById;
+}
+
+async function fetchShowtimeById(movieid) {
+    const apiUrl = `http://localhost:8080/showtimes/${movieid}`;
+
+    const showtimesById = await fetchAnyUrl(apiUrl);
+    return showtimesById;
+}
+
+
+async function getMovieGenres(genre)
+{
+    const apiUrl = `http://localhost:8080/genres/4`;
+    const genresElement = document.getElementById("genre");
+
+    try
+    {
+        const genres = await fetchAnyUrl(apiUrl);
+
+        genres.forEach(genre => {genresElement.appendChild()})
+
+    }
+    catch (error){}
 }
 
 // Function to populate movie details into HTML
@@ -19,7 +61,12 @@ function populateMovieDetails(movie) {
     document.getElementById('movie-picture').innerHTML = `<img src="${movie.picture}" alt="${movie.title}">`;
     document.getElementById('description').innerText = movie.description;
 
-    document.getElementById('movie-details').innerText = `
+    document.getElementById('startDate').innerText = movie.startDate;
+    document.getElementById('endDate').innerText = movie.endDate;
+    document.getElementById('length').innerText = movie.length;
+
+
+        `
         Genre: ${movie.genre} 
         Age Limit: ${movie.ageLimit}
         Length: ${movie.length} 
