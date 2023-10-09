ab2d39650ce43a95a00dea2bdbafaf500f9e92bc
SimonElkhojHess <simo85j7@stud.kea.dk>
2023-10-09 13:55:46 +0200

    made functions to fetch list of all movies and putting them in as suggestions

--- Template/Navbar/navbar.js
+++ Template/Navbar/navbar.js
@@ -9,3 +9,84 @@ function loadNavbar()
         })
         .catch(error => console.error('Error loading navbar:', error))
 }
+
+const searchInput = document.getElementById('searchbar-input');
+const select = document.getElementById('movie-suggestions');
+const searchOptions = document.getElementById('search-options');
+
+let movieArr;
+
+function fetchAllMovies()
+{
+    return fetch('/allmovies').then((response) => response.json());
+}
+
+function fillSuggestions(item)
+{
+    const el = document.createElement("option");
+    el.textContent = item.title;
+    el.value = item.title;
+    select.appendChild(el);
+}
+
+async function fetchMovies(any)
+{
+    select.innerHTML = '';
+    movieArr = await fetchAllMovies();
+    console.log(movieArr);
+    movieArr.forEach(fillSuggestions);
+}
+
+function selectedMovie()
+{
+    const selectedOption = select.options[select.selectedIndex];
+    select.value = selectedOption.value;
+}
+
+function getUserInput()
+{
+    const userInput = searchInput.value;
+    searchOptions.innerHTML = "";
+    if (userInput.length > 0)
+    {
+        movieArr.forEach(option =>
+        {
+            if (option.title.toLowerCase().includes(userInput.toLowerCase()))
+            {
+                const optionIndex = document.createElement('div');
+                optionIndex.textContent = option.title;
+                optionIndex.addEventListener('click', () => createATag(option));
+                searchOptions.appendChild(optionIndex);
+            }
+        })
+    }
+}
+
+function createATag(option)
+{
+    searchInput.value = option.title;
+    searchOptions.innerHTML = "";
+    const aTag = document.createElement('a');
+    aTag.textContent = option.title;
+    aTag.href = option.href;
+    document.body.appendChild(atag);
+
+    removeMovieFromArr(option);
+
+}
+
+function removeMovieFromArr(option)
+{
+    for (let i = 0; i < movieArr.length; i++)
+    {
+        if (movieArr[i].title === option.title)
+        {
+            movieArr.splice(i, 1);
+            break;
+        }
+    }
+}
+
+select.addEventListener('change', selectedMovie);
+searchInput.addEventListener('click', fetchMovies);
+searchInput.addEventListener('input', getUserInput);
