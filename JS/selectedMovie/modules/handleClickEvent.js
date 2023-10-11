1707642eed1d619743113aea3f2a9bb459537b56
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-10-11 11:13:30 +0200

    button now works when clicking text

--- JS/selectedMovie/modules/handleClickEvent.js
+++ JS/selectedMovie/modules/handleClickEvent.js
@@ -4,9 +4,8 @@ export const handleClickEvent = (event) =>
     const target = event.target;
     // const movieId = target.getAttribute('data-movieId');
 
-    if (target.tagName === 'BUTTON')
+    if (target.closest('button'))
     {
         window.location.href = "https://cinemaxx.dk/koebenhavn/kommende-film";
-        // `somepage.html?movie=${movieId}`;
     }
 };
